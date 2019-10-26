cd55a84cdd6736f2f570e2636e53353c0556439f
gaojianbo <419638354@qq.com>
2019-10-26 13:52:29 +0800

    扩展第一次上传

---
+++ test/src/Test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/10/26
+ * Time: 11:13
+ */
+
+namespace test\test;
+
+
+class Test
+{
+    public $password;
+    public function pr(){
+        echo $this->password;
+    }
+
+}
